273a29b5c4ed8942b9575562c137200ecb42f03e
Tim Cuthbertson <tim@gfxmonk.net>
2023-04-12 16:27:04 +1000

    fix chored update

--- choredefs/index.ts
+++ choredefs/index.ts
@@ -1,7 +1,7 @@
 import { defaultDockerOptions, default as Scala } from 'https://raw.githubusercontent.com/timbertson/chored-timbertson/219bc74bb3fdeccb1aba7122e729d211a858061e/lib/scala.ts#main'
 
 export default Scala({ repo: 'sbt-strict-scope',
-		scalaVersion: "2.12.15",
+		scala2Version: "2.12.15",
 		strictPluginOverride:
 			// This repo is the strict plugin, so we directly depend on our own source code
 			`Compile / unmanagedSourceDirectories += baseDirectory.value.getParentFile / "src" / "main" / "scala"`,
